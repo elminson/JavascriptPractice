cbcdefa930d95689ef0391858ac9f953f6ed2d35
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-13 14:43:07 -0400

    chunk/index.js

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const array_result = [];
+  let final = 0;
+  while (final < array.length) {
+    array_result.push(array.slice(final,final+size));
+    final += size;
+  }
 
+  return array_result;
+}
+
+// function chunkv1(array, size) {
+//   const array_result = [];
+//
+//   for (let element of array) {
+//     const last = array_result[array_result.length - 1];
+//
+//     if (!last || last.length === size) {
+//       array_result.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return array_result;
+// }
 module.exports = chunk;
