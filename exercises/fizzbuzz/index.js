c43d12d38ab63bf9916c89d89590cd781eef28f4
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-12 23:53:53 -0400

    fizzbuzz/index.js

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,42 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    // Is the number a multiple of 3 and 5?
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      // Is the number a multiple of 3?
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
+
+// for(let number =1;number<= n; number++){
+//   fizzbuzz = false;
+//   if((number%5 ===0) && (number%3===0)){
+//     console.log("fizzbuzz");
+//     fizzbuzz = true;
+//   } else if(number%3===0){
+//     console.log("fizz");
+//     fizzbuzz = true;
+//   }else
+//   if(number%5 ===0){
+//     console.log("buzz");
+//     fizzbuzz = true;
+//   }
+//
+//   if(!fizzbuzz)
+//   {
+//     console.log(number);
+//   }
+//   //console.log("\n");
+//
+// }
