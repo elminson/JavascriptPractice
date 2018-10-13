ca2c7ba7cf6616c8e07eb7a380b45598ed4a67ee
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-12 23:16:59 -0400

    reverseint/index.js'

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+const rev_num = n.toString().split('').reverse().join('');
+ return parseInt(rev_num) * Math.sign(n);
+}
 
 module.exports = reverseInt;
